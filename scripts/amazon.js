30357db225a099dfed829edc2bfacadacb5c65fd
Vinit <inglevinit84@gmail.com>
2025-08-29 20:33:39 +0530

    used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,62 +1,28 @@
-const product=[
-    {
-        img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating:{
-            stars:4.5,
-            count:87
-        },
-        priceCents:1090
-    },{
-        img:'images/products/intermediate-composite-basketball.jpg',
-        name:' Intermediate Size Basketball',
-        rating:{
-            stars:4,
-            counts:127
-        },
-        priceCents:'2095'
-    },{
-        img:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name:'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating:{
-            stars:4.5,
-            counts:56
-        },
-        priceCents:'799'
-    },{
-        img:'images/products/black-2-slot-toaster.jpg',
-        name:'2 Slot Toaster-black',
-        rating:{
-            stars:5,
-            counts:2197
-        },
-        priceCents:'1899'
-    }
-]
+
 
 let productHTML='';
 
-product.forEach((product)=>{
+products.forEach((products)=>{
    productHTML=productHTML+   `   <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.img}">
+              src="${products.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${product.name}
+            ${products.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${products.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.counts}
+              ${products.rating.counts}
             </div>
           </div>
 
           <div class="product-price">
-            $${product.priceCents / 100}
+            $${products.priceCents / 100}
           </div>
 
           <div class="product-quantity-container">
