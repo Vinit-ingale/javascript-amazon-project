176db6af0a5accf259f089057ade40d402cb24c6
Vinit <inglevinit84@gmail.com>
2025-08-30 22:00:38 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,7 +76,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         quantity:1
       });
       }
-   
-      console.log(cart)
+
+      let cartQuntity=0;
+      cart.forEach((item)=>{
+        cartQuntity+=item.quantity
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML=cartQuntity;
     }); 
 })
